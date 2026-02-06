a0bdad44e78aef0e6357efe735980bd3e4fcd139
aagaman mainali <aagamanmainali35@gmail.com>
2026-02-06 13:26:34 +0545

    Learnt about Classes , attributes and methods in Python

---
+++ Animal.py
@@ -0,0 +1,17 @@
+class Animal:# class is blue print of real world objects or things 
+    def __init__(self,weight , breed):
+        self.breed=breed # initalizing values from arguments
+        self.weight=weight
+
+    def eat(self):print(f'The animal is eating') # class can have many methods that defines its actions such as sleep or eat 
+    
+    def sleep(self):print(f'The animal is sleeping') 
+    
+    def define(self):
+        print(f'the animal is of {self.breed}, and its weight is  {self.weight}')
+    
+Dog=Animal(12,'Husky') # passing arguments to constructor 
+Dog.define()
+
+    
+        
