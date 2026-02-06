f9bd4271847767920920f868fe74b96e251bf405
aagaman mainali <aagamanmainali35@gmail.com>
2026-02-06 13:59:41 +0545

    Multi level inheritance with eaxmpale

---
+++ Car.py
@@ -0,0 +1,40 @@
+class Electric():
+    Total_charge=0
+    def __init__(self):
+        pass    
+    
+    def charge(self,Time):
+        print(f'The car will take {self.Time} to charge fully at {Electric.Total_charge}')
+        
+class fuel():
+    def __init__(self):
+        pass
+   
+    def Refuel(self,time):
+        print(f'The car will take {time} seconds to Fuel fully at {self.fuel_capacity} liters')
+        
+    
+        
+class Car(Electric,fuel):
+    def __init__(self,Brand,Color,milage,speed,Cruise_control,seat,fuel_capacity):
+        self.Brand=Brand
+        self.Color=Color
+        self.milage=milage
+        self.speed=speed
+        self.Cruise_control=Cruise_control
+        self.seat=seat
+        self.fuel_capacity=fuel_capacity
+        
+    def show_sepcs(self):
+        pass
+    
+    def start(self):
+        print(f'The car started')
+        
+    def stop(self):
+        print('The car stopped')
+    
+Honda=Car('Honda','Green',42,130,True,2,500)
+Honda.Refuel(100)
+        
+    
--- Inheritance.py
+++ Inheritance.py
@@ -1,2 +1,29 @@
 from Animal import Animal
  
+class Cat(Animal): # Cat class inheritance from parent class named Animal
+    def __init__(self):
+        pass
+    
+    def meow(self):
+        print(f'The cat meowed')
+    
+
+class Dog(Animal): # One parent can have multiple child so cAnimal is inherited by multiple Child 
+    def __init__(self):
+        pass
+    
+    def bark(self):
+        print(f'The dog barked')
+        
+
+suzu=Cat() # creating an object of class 
+subaru=Dog()
+
+suzu.eat()
+suzu.sleep() # Accesing its methods
+suzu.meow()
+subaru.eat()
+subaru.sleep()
+suzu.run() # AttributeError: 'Cat' object has no attribute 'run' since there is no run metjod in both object dan parent class
+
+    
