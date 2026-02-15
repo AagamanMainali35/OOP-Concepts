63b6d0f9826620acd8877dda1289d7f652285ade
aagaman mainali <aagamanmainali35@gmail.com>
2026-02-15 11:21:33 +0545

    made subclass of Physical product

--- Learning.py/Ecommerce.py
+++ Learning.py/Ecommerce.py
@@ -1,12 +1,44 @@
-class Product:
-    def __init__(self):
-        pass 
-    
+from abc import ABC,abstractmethod
+
 class user:
-    pass
+    def __int__(self,id,name):
+        self.id=id
+        self.name=name
+        self._balance=0
     
-class Physicl(Product):
-    pass
+    @property
+    def balance(self):
+        return self._balance
+    
+    @balance.setter
+    def set_balance(self,new_balance):
+        if new_balance<0:
+            print('Balnace cant be less Than 0 ')
+        self._balance=new_balance
+            
+class Product(ABC):
+    def __init__(self,id,name,price):
+        self.id=id
+        self.name=name
+        self.price=price
+        
+    @abstractmethod
+    def get_price(self):
+        return self.price
+    
+class Physical(Product):
+    def __init__(self, id, name, price, weight):
+        self.weight = weight
+        flat_rate = 250
+        extra_rate = 60
+        if weight <= 4:
+            shipping = flat_rate
+        else:
+            shipping = flat_rate + ((weight - 4) * extra_rate)
+
+        total_price = price + shipping
+        super().__init__(id, name, total_price)
+        
 
 class Digital(Product):
     pass
