5fd11e7a3c4d87420d5744af164f90f4d29a1762
aagaman mainali <aagamanmainali35@gmail.com>
2026-02-17 09:47:25 +0545

    Added member class and its registration and loaded sample data

--- BankTransaction.py/library.py
+++ BankTransaction.py/library.py
@@ -1,3 +1,10 @@
+import random
+import string
+class Memeber:
+    def __init__(self,FullName,ID):
+        self.FullName=FullName
+        self.ID=ID
+
 class Book:
     def __init__(self, BookID, Title, Author, Copies):
         self.BookID = BookID
@@ -5,14 +12,25 @@ class Book:
         self.Author = Author
         self.Copies = int(Copies)
 
-
 class Library:
     def __init__(self, Name):
         self.Name = Name
+        self.member_list=[]
         self.booklist = []
-
+        
+    def register(self,Fullname):
+        id="".join(random.choices(string.ascii_letters+string.digits,k=9))
+        self.member_list.append(Memeber(FullName=Fullname,ID=id))
+        
+    def list_members(self):
+        print("-"*35)
+        print(f"{"SN":<5} {"MemeberID":<15}{"Name"} ")
+        print("-"*35)
+        for index,persons in enumerate(self.member_list):
+            print(f'{index+1:<5}{persons.ID:<15}{persons.FullName}')
+            
     def add_book(self):
-        book_ID = input("Enter book ID: ")
+        book_ID = print("".join(random.choices(string.ascii_letters+string.digits,k=9)))
         book_title = input("Enter book title: ")
         book_author = input("Enter book author: ")
 
@@ -30,14 +48,23 @@ class Library:
             print("No books available.")
             return
         else:
-            print(f'{"SN":<3} {"Book Name":<32} {"Author Name":<30} {"Copies"}')
+            print(f'{"SN":<3} {"Book Name":<32} {"Author Name":<30} {"Available_Copies"}')
             print("-" * 75)
             for i, b in enumerate(self.booklist, 1):
                 print(f"{i:<4} {b.Title[:20]+"...":<30}  {b.Author:<30} {b.Copies}")
-
+              
+    def borrow_Book(self,member_ID,BookID,BorrowingTime):
+        pass
 
 library = Library("MyLibrary")
-# NOTE: For testing purpose only
+# NOTE: Loading sample data before program initialization
+names = [
+    "Aagaman Mainali", "John Doe", "Jane Smith", "Alice Brown", "Bob Johnson",
+    "Charlie Davis", "Diana Evans", "Ethan Foster", "Fiona Green", "George Harris"
+]
+for name in names:
+    library.register(name)
+
 library.booklist.extend([
     Book("B1", "Atomic Habits", "James Clear", 5),
     Book("B2", "Clean Code", "Robert C. Martin", 3),
@@ -56,24 +83,37 @@ library.booklist.extend([
     Book("B15", "Principles", "Ray Dalio", 5)
 ])
 
-
 def main():
     while True:
         print("\n===== Library Menu =====")
-        print("1. Add Book")
-        print("2. Show Available Books")
-        print("3. Quit")
+        print("1. Register as a member")
+        print("2. List all member")
+        print("3. Add Book")
+        print("4. Show Available Books")
+        print("5. Borrow a Book")
+        print("6. Quit")
 
         choice = input("Enter your choice: ")
-        if choice == "1":
-            library.add_book()
-        elif choice == "2":
-            library.list_books()
+        if choice=="1":
+            name=input('Enter Your Full Name : ')
+            library.register(name)
+            print('Memeber registration sucessfull')
+        elif choice=="2":
+            library.list_members()
         elif choice == "3":
+            library.add_book()
+        elif choice == "4":
+            library.list_books()
+        elif choice=="5":
+            pass
+        elif choice == "6":
             print("Thanks for coming!")
             break
         else:
             print("Invalid choice. Try again.")
-
-
+            
 main()
+
+
+
+
